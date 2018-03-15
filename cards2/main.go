3c631fbb75f3f2b9834462d2c9c3299c9d13cd99
IndustrialRobotics <andy@cristaleja.net>
2018-03-15 18:29:08 -0400

    Wrote two new functions: newCardFromFile and writeToFile

--- cards2/main.go
+++ cards2/main.go
@@ -1,20 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	//cards := newCard()
-	////cards.print()
-	//
-	//hand, remainingDeck := deal(cards, 4)
-	//hand.print()
-	//remainingDeck.print()
-	//
-	//
-	//greeting := "Hello There"
-	//fmt.Println([]byte(greeting))
-
-	cards := newCard()
-	fmt.Println(cards.toString())
+	cards := newDeckFromFile("my_Cards")
+	cards.print()
 }
